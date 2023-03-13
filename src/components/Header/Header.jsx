73d8e1a9cee38f396da53e9e223851956e52d99b
romeuic <casarottori@gmail.com>
2023-03-13 20:21:56 -0300

    adiciona exemplo context api

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import { useContext } from "react";
+import UserContext from "../../UserContext";
+import "./Header.css";
+
+function Header() {
+  const userInfo = useContext(UserContext);
+
+  return (
+    <header className="HeaderContainer">
+      <h1>Módulo 2 Semana 9</h1>
+      <p>{userInfo?.name ?? "Sem nome"}</p>
+    </header>
+  );
+}
+
+export default Header;
